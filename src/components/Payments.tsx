70cae6b0562d0834fff4aad4ce001e9980e62360
jussiisokangas <ext-jussi.isokangas@elisa.fi>
2025-01-31 09:42:25 +0200

    fix: add payment headers for groups

--- src/components/Payments.tsx
+++ src/components/Payments.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@mui/joy";
+import { Card, Typography } from "@mui/joy";
 import { usePayment } from "../fetchData";
 import { Customer } from "../features/api/types";
 import { Suspense } from "react";
@@ -9,12 +9,12 @@ export const Payments = () => {
   const customer: Customer =
     localStoreCustomer !== null ? JSON.parse(localStoreCustomer) : null;
 
-  const {data} = usePayment(customer);
+  const { data } = usePayment(customer);
   //TODO add paytrail stamp to localstore to confirm on success pay right customer
   return (
     <>
       <div className="disclaimer">
-        <span >
+        <span>
           {" "}
           Valitsemalla maksutavan hyväksyt{" "}
           <a
@@ -25,21 +25,34 @@ export const Payments = () => {
           </a>{" "}
         </span>
       </div>
-      <Suspense fallback={<span>Hetkinen...</span>}>
-      <Card>
-        {data?.providers.map((provider) => (
-          <form method="POST" action={provider.url}>
-            {provider.parameters.map((param) => (
-              <input type="hidden" name={param.name} value={param.value} />
-            ))}
 
-            <button>
-              <img src={provider.icon} />
-            </button>
-          </form>
-        ))}
-      </Card>
-    </Suspense>
+      <Suspense fallback={<span>Hetkinen...</span>}>
+        <Typography fontSize="25px">Valitse maksutapa</Typography>
+        <Card>
+          {data?.groups.map((group) => (
+            <>
+              <Typography fontWeight="1000">{group.name}</Typography>
+              {data?.providers.map((provider) =>
+                group.id === provider.group ? (
+                  <form method="POST" action={provider.url}>
+                    {provider.parameters.map((param) => (
+                      <input
+                        type="hidden"
+                        name={param.name}
+                        value={param.value}
+                      />
+                    ))}
+
+                    <button>
+                      <img src={provider.icon} />
+                    </button>
+                  </form>
+                ) : null
+              )}
+            </>
+          ))}
+        </Card>
+      </Suspense>
     </>
   );
 };
